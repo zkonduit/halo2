bf9d0057a82443be48c4779bbe14961c18fb5996
Artem <48178500+ElusAegis@users.noreply.github.com>
2025-01-20 14:39:10 +0000

    feat: msm metal gpu acceleration (#20)
    This PR introduces support for Metal-based MSM GPU acceleration,
    leveraging the [mopro GPU acceleration
    fork](https://github.com/ElusAegis/metal-msm-gpu-acceleration).
    
    ### Key Highlights:
    - **Performance Gains**: 
    - Expected performance improvements of approximately **25% for log 17**
    instance sizes.
      - Gains increase to **50% or more for log 20+** instance sizes.
    - Detailed benchmarking results are available in the [mopro GPU
    acceleration
    fork](https://github.com/ElusAegis/metal-msm-gpu-acceleration).
    
    - **Selective Activation**:
    - The GPU acceleration is feature gated and further will only be enabled
    **only for MSM operations** with an instance size of **log 17 or
    greater**.
    - A user warning is displayed to inform about the activation threshold
    and expected benefits.
    
    ### Notes:
    This enhancement provides a significant boost for large-scale MSM
    computations, by collectively utilizing Metal's GPU resources with CPU
    resources on supported devices.

--- halo2_proofs/src/arithmetic.rs
+++ halo2_proofs/src/arithmetic.rs
@@ -32,20 +32,38 @@ where
 }
 
 /// Best MSM
-pub fn best_multiexp<C: CurveAffine>(
-    coeffs: &[C::Scalar], bases: &[C]
-) -> C::Curve {
+pub fn best_multiexp<C: CurveAffine>(coeffs: &[C::Scalar], bases: &[C]) -> C::Curve {
     #[cfg(feature = "icicle_gpu")]
     if env::var("ENABLE_ICICLE_GPU").is_ok()
         && !icicle::should_use_cpu_msm(coeffs.len())
         && icicle::is_gpu_supported_field(&coeffs[0])
     {
-        best_multiexp_gpu(coeffs, bases)
-    } else {
-        best_multiexp_cpu(coeffs, bases)
+        return best_multiexp_gpu(coeffs, bases);
     }
 
-    #[cfg(not(feature = "icicle_gpu"))]
+    #[cfg(feature = "metal")]
+    {
+        use mopro_msm::metal::abstraction::limbs_conversion::h2c::{H2Fr, H2GAffine, H2G};
+        use std::sync::Once;
+
+        // Static mutex to block concurrent Metal acceleration calls
+        static PRINT_ONCE: Once = Once::new();
+
+        // Print the warning message only once
+        PRINT_ONCE.call_once(|| {
+            log::warn!(
+                "WARNING: Using Experimental Metal Acceleration for MSM. \
+                 Best performance improvements are observed with log row size >= 20. \
+                 Current log size: {}",
+                coeffs.len().ilog2()
+            );
+        });
+
+        // Perform MSM using Metal acceleration
+        return mopro_msm::metal::msm_best::<C, H2GAffine, H2G, H2Fr>(coeffs, bases);
+    }
+
+    #[allow(unreachable_code)]
     best_multiexp_cpu(coeffs, bases)
 }
 
