4045ea7921f3b5a0a73834aa02aa9e9d6f02ecb8
dante <dantino.11@icloud.com>
2025-08-05 08:28:13 -0400

    feat: allow assignment of values from previous phase to next phase

--- halo2_proofs/src/plonk/prover.rs
+++ halo2_proofs/src/plonk/prover.rs
@@ -239,8 +239,10 @@ where
             A: FnOnce() -> AR,
             AR: Into<String>,
         {
-            // Ignore assignment of advice column in different phase than current one.
-            if self.current_phase != column.column_type().phase {
+            // Ignore assignment of advice column in later phase than current one.
+            // Okay: Assigning Phase 1 value to Phase 2 column
+            // Not okay: Assigning Phase 2 value to Phase 1 column
+            if self.current_phase > column.column_type().phase {
                 return Ok(());
             }
 
