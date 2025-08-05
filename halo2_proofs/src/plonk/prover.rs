a5d352d8472dd1b453f3efaed9a84a6ccfdf73f4
DoHoonKim <dohoon1097819@gmail.com>
2025-08-05 18:56:27 +0900

    Do not skip assigning values even phases do not match
    Co-authored-by: therealyingtong <yingtong.lai@gmail.com>

--- halo2_proofs/src/plonk/prover.rs
+++ halo2_proofs/src/plonk/prover.rs
@@ -239,8 +239,10 @@ where
             A: FnOnce() -> AR,
             AR: Into<String>,
         {
-            // Ignore assignment of advice column in different phase than current one.
-            if self.current_phase != column.column_type().phase {
+            // Okay: Assigning Phase 1 value to Phase 2 column
+            // Not okay: Assigning Phase 2 value to Phase 1 column
+            // TODO : Should ensure that assigning phase 2 value to phase 1 column is prohibited
+            if self.current_phase < column.column_type().phase {
                 return Ok(());
             }
 
